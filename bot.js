21c613658351553af899345cfac6c9a6a0b67455
whitesr09 <96966266+whitesr09@users.noreply.github.com>
2022-01-02 10:45:16 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -391,7 +391,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~❤️WHITE-SR💙~*' +
+                                await conn.sendMessage(conn.user.jid, '*~❤️MAALUTTY💙~*' +
                                     '\n\n*🧞‍♂️ ' + error + '*\n\n```Report🥺 errors\njoin with \n https://chat.whatsapp.com/KJWUNpT7lBzDuPGnlV69Vp``` ' 
                                     , MessageType.text);
                             }
